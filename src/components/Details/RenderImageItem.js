4bf860f4fc3f00a2a07f98033783125836ae4ef7
Long Nguyen <vielongt52@gmail.com>
2023-08-14 11:01:32 +0700

    FastImage applied to RenderImageItem

--- src/components/Details/RenderImageItem.js
+++ src/components/Details/RenderImageItem.js
@@ -1,5 +1,7 @@
-import { View, Image, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import FastImage from 'react-native-fast-image';
 
 import { COLORS, SIZES } from '../../constants';
 
@@ -16,9 +18,12 @@ const styles = StyleSheet.create({
 function RenderImageItem({ item }) {
 	return (
 		<View style={styles.imgContainer}>
-			<Image
+			<FastImage
 				// Move the key prop to the Image component
-				source={{ uri: item }}
+				source={{
+					uri: item,
+					priority: FastImage.priority.normal,
+				}}
 				style={{ width: '100%', height: '100%', resizeMode: 'contain' }}
 			/>
 		</View>
